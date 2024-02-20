2313d384b3bb5ef90e5d136a2b4533ada8e51f15
riohadi26 <159427175+riohadi26@users.noreply.github.com>
2024-02-20 19:48:09 +0700

    Update main.py

--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,9 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+
+harga_awal = 37000
+diskon = 15/100
+harga_diskon = harga_awal * diskon
+harga_akhir = harga_awal - harga_diskon
+print(harga_akhir)
